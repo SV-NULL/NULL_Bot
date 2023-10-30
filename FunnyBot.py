cffa71b253fda4a03bcc7cac7a12d3fc40e9fa86
B Vos <ben.j.vos@gmail.com>
2023-10-30 16:02:29 +0100

    Created an expandable logic structure

--- FunnyBot.py
+++ FunnyBot.py
@@ -20,29 +20,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if cliengt.user in message.mentions:
-        if "anime" in message.content:
-            if "gif" in message.content:
-                with open('Messages/AnimeGifs.txt', 'r') as ag:
-                    responses = [line.strip() for line in ag]
-                    response = random.choice(responses)
-                    await message.channel.send(response)
-            else:
-                with open('Messages/Anime.txt', 'r') as a:
-                    responses = [line.strip() for line in a]
-                    response = random.choice(responses)
-                    await message.channel.send(response)
-        if "null" in message.content or "Null" in message.content or "NULL" in message.content:
-            if "gif" in message.content:
-                with open('Messages/NULLGifs.txt', 'r') as ng:
-                    responses = [line.strip() for line in ng]
-                    response = random.choice(responses)
-                    await message.channel.send(response)
-            else:
-                with open('Messages/NULLResponses.txt', 'r') as f:
-                    responses = [line.strip() for line in f]
-                    response = random.choice(responses)
-                    await message.channel.send(response)
+    if cliengt.user not in message.mentions:
+        return
+    
+    triggers = ['anime', 'null']
+    query = str(message.content).lower()
+    response = ''
 
+    for searchterm in triggers: # Loop through the triggers
+        if searchterm in query:
+            isGif = 'gif' in query
+            response = getResponse(searchterm, isGif)
+            break
+
+    if response: await message.channel.send(response)
+
+def getResponse(filename, isGif):
+    if isGif: filename = filename + 'Gifs'
+    else: filename = filename + 'Responses'
+        
+    with open('Messages/{}.txt'.format(filename), 'r') as file:
+        responses = file.readlines()
+        randomResponse = random.choice(responses).strip()
+        return randomResponse
+     
 
 client.run(TOKEN)
