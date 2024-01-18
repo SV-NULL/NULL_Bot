a302445081a0558dbb8dfbc4573c7bb5fbc326ab
Big-Nugget <116891680+Big-Nugget@users.noreply.github.com>
2024-01-18 15:23:17 +0100

    Bobr?

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ const client = ClientFile.GetClient();
 
 client.once("ready", async () => {
     console.log(client.user.tag + " is online");
-    client.user.setActivity("['null', 'null gif', 'anime', 'anime gif'] to get a response", { type: discord.ActivityType.Watching })
+    client.user.setActivity("['null', 'null gif', 'anime', 'anime gif', 'bobr'] to get a response", { type: discord.ActivityType.Watching })
 })
 
 const nullText = require("./messageResponses/NULLResponses.json")
 const nullGif = require("./messageResponses/NULLGifs.json")
 const animeText = require("./messageResponses/Anime.json");
 const animeGif = require("./messageResponses/AnimeGifs.json");
+const bobrGif = require("./messageResponse/BobrGifs.json");
 
 client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
@@ -25,9 +26,11 @@ client.on("messageCreate", async (message) => {
     const isAnime = msgContent.includes("anime");
     const isNull = msgContent.includes("null");
     const isGif = msgContent.includes("gif");
+    const isBobr = msgContent.includes("bobr");
 
     if (isAnime && isGif) return replyToMessage(message, getRandomLine(animeGif));
     if (isNull && isGif) return replyToMessage(message, getRandomLine(nullGif));
+    if (isBobr) return replyToMessage(message, getRandomLine(bobrGif));
 
     if (isAnime) return replyToMessage(message, getRandomLine(animeText));
     if (isNull) return replyToMessage(message, getRandomLine(nullText));
